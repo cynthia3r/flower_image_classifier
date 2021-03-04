165a5603f74e46da285f84571318d2f3ff51712f
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-03-04 20:48:29 +1100

    feat: check for cuda availability, correct conversion applied in predict function

--- predict.py
+++ predict.py
@@ -31,17 +31,25 @@ results = parser.parse_args()
 image_dir = results.image_dir
 model_input = results.model_input
 top_k = results.top_k
-category_names = results.category_names
-device = results.device
 
+if results.device == 'cuda' and torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('CUDA GPU mode is enabled now')
+elif results.device == 'cpu':
+    device = torch.device('cpu')
+    print('CPU mode is enabled now')
+else:
+    print('CUDA GPU is not supported on this system so switching to CPU mode')
+    device = torch.device('cpu')
+
+category_names = results.category_names
+with open(category_names, 'r') as f:
+    cat_to_name = json.load(f)
 
 model, optimizer = load_checkpoint(model_input)
 
 for param in model.parameters():
     param.requires_grad = False
-
-with open(category_names, 'r') as f:
-    cat_to_name = json.load(f)
 
 
 # Class Prediction (The predict function successfully takes the path to an image and a checkpoint,
@@ -56,32 +64,31 @@ def predict(image_dir, model, topk, device):
     model.eval()
 
     image_tensor = process_image(image_dir)
-    image_tensor = image_tensor.to('cuda')
+    image_tensor = image_tensor.to(device)
 
     with torch.no_grad():
         output = model.forward(image_tensor)
 
     ps = torch.exp(output)
     # Top K classes: the function predicts the top K classes along with associated probabilities
-    probs = np.array(ps.topk(topk)[0])[0]
+    probs_topk = np.array(ps.topk(topk)[0])[0]
     class_idx = np.array(ps.topk(topk)[1])[0]
 
-    # Return preconverted numpy array from a tensor torch along with the image tensor
-    return image_tensor, probs, class_idx
+    # Invert the model.class_to_idx dict to obtain mapping
+    idx_to_class = dict((value, key) for key, value in model.class_to_idx.items())
+    class_topk = []
+    for idx in class_idx:
+        class_topk.append(idx_to_class[idx])
+
+    return image_tensor, probs_topk, class_topk
 
 
 # Predicting classes: The predict function successfully reads in an image and a checkpoint
-# then returns the most likely image class and it's associated probability
+# then returns the most likely image class index and it's associated probability
 # Run prediction function
-image_tensor, probs, class_idx = predict(image_dir, model, top_k, device)
-print(probs)
-print(class_idx)
+image_tensor, probs_topk, class_topk = predict(image_dir, model, top_k, device)
 
-# Retrieve the flowername from the cat_to_name dictionary
-# Displaying class names: the script maps the class values to category names based on loaded json mapping in cat_to_name
-idx_to_flower_name = {category: cat_to_name[str(category)] for category in class_idx}
-predicted_flower_names = list(idx_to_flower_name.values())
-
-print("Predicted Flower Category Name = {}".format(predicted_flower_names))
-print("Predicted probability = {}".format(probs))
-print("The model predicted flower name as {} with {}% probability".format(predicted_flower_names[0], 100 * probs[0]))
+predicted_flower_names = [cat_to_name[idx] for idx in class_topk]
+print("{} most likely predicted flower names = {}".format(top_k, predicted_flower_names))
+print("{} most likely predicted probability = {}".format(top_k, probs_topk))
+print("The model predicted flower name as {} with {:.3f}% probability".format(predicted_flower_names[0], 100 * probs_topk[0]))
